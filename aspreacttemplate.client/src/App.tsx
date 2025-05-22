a60dc62b08670f15bf50cfbbf3304aaa6997ed66
HajekM <martin.hajek@kamax.com>
2025-05-22 11:33:41 +0200

    update my own

--- aspreacttemplate.client/src/App.tsx
+++ aspreacttemplate.client/src/App.tsx
@@ -1,50 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
 import TestPage from './pages/TestPage';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<string>('home');
-  const [forecasts, setForecasts] = useState<any[]>([]);
+    const [currentPage, setCurrentPage] = useState<string>('home');
 
-  useEffect(() => {
-    // Keep the weather forecast data fetching for reference
-    populateWeatherData();
-  }, []);
+    const handleNavigate = (page: string) => {
+        setCurrentPage(page);
+    };
 
-  const handleNavigate = (page: string) => {
-    setCurrentPage(page);
-  };
+    // Render the appropriate page based on navigation state
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'home':
+                return <HomePage />;
+            case 'test':
+                return <TestPage />;
+            default:
+                return <HomePage />;
+        }
+    };
 
-  // Render the appropriate page based on navigation state
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <HomePage />;
-      case 'test':
-        return <TestPage />;
-      default:
-        return <HomePage />;
-    }
-  };
-
-  async function populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    if (response.ok) {
-      const data = await response.json();
-      setForecasts(data);
-    }
-  }
-
-  return (
-    <div className="d-flex">
-      <Sidebar activeLink={currentPage} onNavigate={handleNavigate} />
-      <div className="content-area flex-grow-1 overflow-auto">
-        {renderPage()}
-      </div>
-    </div>
-  );
+    return (
+        <div className="d-flex">
+            <Sidebar activeLink={currentPage} onNavigate={handleNavigate} />
+            <div className="content-area flex-grow-1 overflow-auto">
+                {renderPage()}
+            </div>
+        </div>
+    );
 }
 
 export default App;
